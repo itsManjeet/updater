232455037b81be6ccb54448446b3b4668fdfd15d
itsmanjeet <itsmanjeet1998@gmail.com>
2020-01-18 16:00:12 +0530

    added updated sub command

--- src/main.cc
+++ src/main.cc
@@ -16,14 +16,22 @@ main(int    ac,
 
        ->author("Manjeet Saini","itsmanjeet1998@gmail.com","-")
        ->config_file(CONF_FILE)
-       
-    /* start Service if no args */
+
+    /* start Service if no args 
+       docit: to start service  */
        ->main_func(func_service)
 
+    /* docit: to check for updates*/
        ->sub("check",
              "check for new updates",
              "",
-             func_check);
+             func_check)
+
+   /* docit: to do updates */
+       ->sub("update",
+             "do upgrade to latest release",
+             "",
+             func_do_update);
 
 
     int status = app->execute(ac, av);
